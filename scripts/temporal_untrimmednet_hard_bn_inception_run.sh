8e271773f640821b5537528d8aba6ba0eafb4986
lmwang <lmwang@gmail.com>
2017-09-14 16:13:10 +0800

    add temporal stream

---
+++ scripts/temporal_untrimmednet_hard_bn_inception_run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+TOOLS=/home/lmwang/code/caffe_cvpr16/cmake_build_mem/install/bin
+
+/usr/local/openmpi/bin/mpirun -n 8 \
+$TOOLS/caffe train --solver=../models/temporal_untrimmednet_hard_bn_inception_solver.prototxt --weights=bn_inception_flow_init.caffemodel
+    
+echo "Done."
+
