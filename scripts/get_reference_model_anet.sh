757e988a776700abae2d90cb6e345897dbb941a7
lmwang <lmwang@gmail.com>
2017-09-19 15:41:20 +0800

    add reference model

---
+++ scripts/get_reference_model_anet.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+
+wget -O models/anet1.2_spatial_untrimmednet_hard_bn_inception.caffemodel http://mmlab.siat.ac.cn/untrimmednet_model/anet1.2_spatial_untrimmednet_hard_bn_inception.caffemodel
+wget -O models/anet1.2_spatial_untrimmednet_soft_bn_inception.caffemodel http://mmlab.siat.ac.cn/untrimmednet_model/anet1.2_spatial_untrimmednet_soft_bn_inception.caffemodel
+wget -O models/anet1.2_temporal_untrimmednet_hard_bn_inception.caffemodel http://mmlab.siat.ac.cn/untrimmednet_model/anet1.2_temporl_untrimmednet_hard_bn_inception.caffemodel
+wget -O models/anet1.2_temporal_untrimmednet_soft_bn_inception.caffemodel http://mmlab.siat.ac.cn/untrimmednet_model/anet1.2_temporal_untrimmednet_soft_bn_inception.caffemodel
+
+
+
+
+
