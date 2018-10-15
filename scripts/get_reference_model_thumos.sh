b497f049c85fe14a183b98721e23141161dd5564
Limin Wang <lmwang.nju@gmail.com>
2018-10-15 10:51:45 +0800

    Update get_reference_model_thumos.sh
    Update thumos model link.

--- scripts/get_reference_model_thumos.sh
+++ scripts/get_reference_model_thumos.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 
-wget -O models/thumos14_spatial_untrimmednet_hard_bn_inception.caffemodel http://mmlab.siat.ac.cn/untrimmednet_model/thumos14_spatial_untrimmednet_hard_bn_inception.caffemodel
-wget -O models/thumos14_spatial_untrimmednet_soft_bn_inception.caffemodel http://mmlab.siat.ac.cn/untrimmednet_model/thumos14_spatial_untrimmednet_soft_bn_inception.caffemodel
-wget -O models/thumos14_temporal_untrimmednet_hard_bn_inception.caffemodel http://mmlab.siat.ac.cn/untrimmednet_model/thumos14_temporl_untrimmednet_hard_bn_inception.caffemodel
-wget -O models/thumos14_temporal_untrimmednet_soft_bn_inception.caffemodel http://mmlab.siat.ac.cn/untrimmednet_model/thumos14_temporal_untrimmednet_soft_bn_inception.caffemodel
+wget -O models/thumos14_spatial_untrimmednet_hard_bn_inception.caffemodel http://mcg.nju.edu.cn/models/UntrimmedNet/thumos14_spatial_untrimmednet_hard_bn_inception.caffemodel
+wget -O models/thumos14_spatial_untrimmednet_soft_bn_inception.caffemodel http://mcg.nju.edu.cn/models/UntrimmedNet/thumos14_spatial_untrimmednet_soft_bn_inception.caffemodel
+wget -O models/thumos14_temporal_untrimmednet_hard_bn_inception.caffemodel http://mcg.nju.edu.cn/models/UntrimmedNet/thumos14_temporl_untrimmednet_hard_bn_inception.caffemodel
+wget -O models/thumos14_temporal_untrimmednet_soft_bn_inception.caffemodel http://mcg.nju.edu.cn/models/UntrimmedNet/thumos14_temporal_untrimmednet_soft_bn_inception.caffemodel
 
 
 
